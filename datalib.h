a2e50b99b20dbe75fa97115d4ed35b2bae690f1e
Peter Deak <hyper80@gmail.com>
2020-09-09 18:20:40 +0200

    Add gsafe.pri file

--- datalib.h
+++ datalib.h
@@ -36,7 +36,7 @@
 #include "gstexts.h"
 
 /** The version of gsafe */
-#define GSAFE_VERSION   "1.3.24"
+#define GSAFE_VERSION   "1.3.25"
 
 // ///////////////////////////////////
 // BEGIN - CONFIG/MODIFIERS/MODULES //
