7c2e7dc59c9a742d8bd06411f9acbfac70870138
Peter Deak <hyper80@gmail.com>
2022-12-09 12:24:35 +0100

    Updated version

--- data.h
+++ data.h
@@ -35,7 +35,7 @@
 /*  @{  */
 
 /** The version of gsafe */
-#define GSAFE_VERSION   "2.0.61"
+#define GSAFE_VERSION   "2.0.62"
 
 /** Converts a double value to char * string
  *  @param v the double value to convert
